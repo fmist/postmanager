029348df094b9d856cc33cfbdc0adf656f4e971c
fmist <olejnec@gmail.com>
2022-11-10 20:53:30 +0300

    1011

--- src/test/java/com/rustcohle/postmanager/PostmanagerApplicationTests.java
+++ src/test/java/com/rustcohle/postmanager/PostmanagerApplicationTests.java
@@ -1,13 +1,33 @@
 package com.rustcohle.postmanager;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 @SpringBootTest
 class PostmanagerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void add() {
+		$(By.id("add")).click();
+		$(By.id("title")).val("1и");
+		$(By.id("add")).click();
+		$(By.id("validate_title")).shouldHave(text("Заголовок должен содержать минимум 3 символа"));
+		$(By.id("title")).val("q");
+		$(By.id("add")).click();
+		$(By.id("validate_text")).shouldHave(text("Описание не может быть пустым"));
+		$(By.id("validate_title")).shouldNotBe(visible);
+		$(By.id("text")).val("jf");
+		$(By.id("add")).click();
+		$(By.id("validate_text")).shouldHave(text("Описание не может быть пустым"));
+		$(By.id("validate_title")).shouldNotBe(visible);
+		$(By.id("text")).val("sw");
+		$(By.id("add")).click();
 	}
 
 }
---
+++ src/test/java/com/rustcohle/postmanager/hooks/GlobalHooks.java
@@ -0,0 +1,19 @@
+package com.rustcohle.postmanager.hooks;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.After;
+import org.junit.Before;
+
+public class GlobalHooks {
+
+    @Before
+    public void init() {
+        Configuration.baseUrl="https://192.168.0.110:8083";
+    }
+
+    @After
+    public void stop() {
+        Selenide.closeWebDriver();
+    }
+}
